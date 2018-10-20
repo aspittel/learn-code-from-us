f9ab440a184180cd700aec7c653e739e1f007a9e
nobuti <buti@nobuti.com>
2018-10-20 12:55:52 +0200

    Header tests

---
+++ src/Header/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Link } from "react-router-dom"
+import Header from './index'
+
+describe('Header', () => {
+  it('render properly', () => {
+    const wrapped = shallow(<Header />)
+    expect(wrapped.find('a').length).toBe(1)
+    expect(wrapped.find(Link).length).toBe(1)
+  })
+
+  it('should include a link to home view', () => {
+    const wrapped = shallow(<Header />)
+    expect(wrapped.find('a').prop('href')).toBe('/')
+  })
+
+  it('should include a link to about view', () => {
+    const wrapped = shallow(<Header />)
+    expect(wrapped.find(Link).prop('to')).toBe('/about')
+  })
+})
+
